2c3dbb0f387986ec3f18c927e3335aee82d7906b
Mr Booi <ayabongabooi2@gmail.com>
2018-07-23 17:53:49 +0200

    database tables created

--- public/waiterdb.sql
+++ public/waiterdb.sql
@@ -1,4 +1,4 @@
-drop table if exists waiterDB, availableDays  ;
+drop table if exists waiterDB, weekdays,shifts;
 
 create table waiterDB(
 id serial not null primary key,
@@ -7,7 +7,7 @@ full_name text not null,
 position text not null
 );
 
-CREATE TABLE Wednesdays(
+CREATE TABLE weekdays(
  id serial not null PRIMARY KEY ,
   dayName VARCHAR(20)
 );
@@ -17,18 +17,21 @@ id serial not NULL PRIMARY key,
 waiter_id  int not null 
  foreign key (waiter_id) references waiterDB(id),
 weekday_id int not null 
-foreign key (weekday_id) references Wednesdays(id)
+foreign key (weekday_id) references weekdays(id)
 );
-
 
 
 --  INSERT data into waiter
 INSERT INTO waiterDB (username,full_name,position) VALUES ('Av','Aviwe boss','waiter');
 INSERT INTO waiterDB (username,full_name,position) VALUES ('Mrsono','Luvuyo sono','waiter');
 INSERT INTO waiterDB (username,full_name,position) VALUES ('Mrbooi','Ayabonga Booi','Admin');
--- seleting shift days
+-- INSERT WEEKDAYS
+INSERT INTO weekdays (dayName) VALUES ('Sunday');
+INSERT INTO weekdays (dayName) VALUES ('Monday');
+INSERT INTO weekdays (dayName) VALUES ('Tuesday');
+INSERT INTO weekdays (dayName) VALUES ('Wednesday');
+INSERT INTO weekdays (dayName) VALUES ('Thursday');
+INSERT INTO weekdays (dayName) VALUES ('Friday');
+INSERT INTO weekdays (dayName) VALUES ('Saturday');
 
 
-
-
--- INSERT into waiters 
